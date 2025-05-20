97c00eb5fd81b887eb355746081814546e31b359
Timina Makena <timina.makena@student.moringaschool.com>
2025-05-20 12:44:03 +0300

    add customer file

---
+++ customer.py
@@ -0,0 +1,19 @@
+class Customer:
+    def __init__(self, name):
+       
+       
+        self.name = name
+        self._orders = []
+    
+    def orders(self):
+        
+        return self._orders
+    
+    def coffees(self):
+       
+        return list(set(order.coffee for order in self._orders))
+    
+    def create_order(self, coffee, price):
+        
+        from order import Order
+        return Order(self, coffee, price)
